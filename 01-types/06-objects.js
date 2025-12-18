6b2c16e15aab30f90e785996a495deb9c6b6f0ea
Ramos <andfera201@gmail.com>
2025-12-17 22:03:55 -0500

    objects

---
+++ 01-types/06-objects.js
@@ -0,0 +1,20 @@
+let name = "Andres"
+let age = 32
+let note = 1
+
+let person = {
+    name: "Andres",
+    age: 32,
+    note: 1,
+}
+
+console.log(person)
+console.log(person.name)
+console.log(person['age'])
+
+person.note = 5
+console.log(person.note)
+
+delete person.note
+console.log(person)
+
