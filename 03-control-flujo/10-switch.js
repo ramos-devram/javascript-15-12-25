605a2586447650e15d095a09d76a5e2ff150128e
Ramos <andfera201@gmail.com>
2026-01-05 08:53:54 -0500

    switch

---
+++ 03-control-flujo/10-switch.js
@@ -0,0 +1,12 @@
+let action = "Read"
+
+switch (action) {
+    case "Read":
+        console.log("Action: Read")
+        break
+    case "Save":
+        console.log("Action: Save")
+        break
+    default:
+        console.log("No existe la acción")
+}
